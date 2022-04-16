01a8e8180d8baee3971959cea69a2aabb69a36f7
allexvip <alexey_shes@mail.ru>
2022-04-16 19:43:46 +0300

    fix

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import logging
 
 from aiogram import Bot, Dispatcher, executor, types
-from aiogram.types import ReplyKeyboardRemove, ReplyKeyboardMarkup, KeyboardButton, InlineKeyboardMarkup, InlineKeyboardButton
+from aiogram.types import ReplyKeyboardRemove, ReplyKeyboardMarkup, KeyboardButton, InlineKeyboardMarkup, \
+    InlineKeyboardButton
 from dotenv import dotenv_values
 
 config = dotenv_values(".env")
@@ -15,14 +16,17 @@ logging.basicConfig(level=logging.INFO)
 bot = Bot(token=API_TOKEN)
 dp = Dispatcher(bot)
 
-markup = ReplyKeyboardMarkup().add(KeyboardButton(text='Отправьте мне свой контакт',request_contact=True))
+markup = ReplyKeyboardMarkup().add(KeyboardButton(text='Отправьте мне свой контакт', request_contact=True))
+
 
 @dp.message_handler(commands=['start', 'help'])
 async def send_welcome(message: types.Message):
     """
     This handler will be called when user sends `/start` or `/help` command
     """
-    await message.answer("{1}, бот {0} приветствует вас.\n\n Жмите /new".format(config['BOT_NAME'],message.from_user.first_name))
+    await message.answer(
+        "{1}, бот {0} приветствует вас.\n\n Жмите /new".format(config['BOT_NAME'], message.from_user.first_name))
+
 
 @dp.message_handler(commands=['new'])
 async def send_new(message: types.Message):
@@ -30,6 +34,7 @@ async def send_new(message: types.Message):
     new command
     """
     await message.answer("Пожалуйста предоставьте боту свой номер телефона", reply_markup=markup)
+
 
 @dp.message_handler(content_types=['contact'])
 async def contact(message):
