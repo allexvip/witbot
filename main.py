778b38ed5489dbc407db56d8a98c70b2d89ebaa0
allexvip <alexey_shes@mail.ru>
2022-04-16 21:05:19 +0300

    fix

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import logging
-
+import requests
 from aiogram import Bot, Dispatcher, executor, types
 from aiogram.types import ReplyKeyboardRemove, ReplyKeyboardMarkup, KeyboardButton, InlineKeyboardMarkup, \
     InlineKeyboardButton
@@ -17,6 +17,20 @@ bot = Bot(token=API_TOKEN)
 dp = Dispatcher(bot)
 
 markup = ReplyKeyboardMarkup().add(KeyboardButton(text='Отправьте мне свой контакт', request_contact=True))
+
+
+async def get_phone_info(phone):
+    r = requests.get('{0}?phone={1}&token={2}'.format(config['PHONE_CHECK_URL'], phone, config['PHONE_CHECK_TOKEN']))
+    return r
+
+
+async def check_phone_number(phone):
+    result = {}
+    r = requests.get('https://zniis.ru/bdpn/check/?num={0}'.format(phone[2:]))
+    html = r.text
+    result['operator'] = html.split('Оператор: ')[1].split('<br>')[0]
+    result['region'] = html.split('Регион: ')[1].split('"')[0]
+    return result
 
 
 @dp.message_handler(commands=['start', 'help'])
@@ -41,11 +55,12 @@ async def contact(message):
     if message.contact is not None:
         markup = types.ReplyKeyboardRemove()
         await bot.send_message(message.chat.id, 'Вы успешно отправили свой номер', reply_markup=markup)
-        global phonenumber
-        phonenumber = str(message.contact.phone_number)
-        user_id = str(message.contact.user_id)
-        await message.answer('Ваш номер телефона: {0}'.format(phonenumber))
-        await message.answer('Я сейчас позвоню Вам на номер {0}'.format(phonenumber))
+        res = await get_phone_info(message.contact.phone_number)
+        await message.answer('Ваш номер телефона: {0}'.format(message.contact.phone_number))
+        await message.answer(res.text)
+        phone_info = await check_phone_number(message.contact.phone_number)
+        await message.answer('{0}\n{1}'.format(phone_info['opertor'],phone_info['region']))
+        # await message.answer('Я сейчас позвоню Вам на номер {0}'.format(phonenumber))
 
 
 if __name__ == '__main__':
