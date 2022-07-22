f389663d80f838d7c364c425a892f52dff63cab1
allexvip <alexey_shes@mail.ru>
2022-07-22 19:30:46 +0300

    fix

--- run_bot.py
+++ run_bot.py
@@ -384,7 +384,7 @@ async def send_forward(message: types.Message):
             os.system(f"/opt/cprocsp/bin/amd64/cryptcp -sign -detach './{client_user_info['local_video_in_file_path']}' './{client_user_info['local_video_in_file_path']}.sig'")
             await bot.send_video(int(client_user_info['chatid']),
                                  caption=f"Готовим цифровую подпись к этому видео.",
-                                 video=client_user_info[message.video.file_id])
+                                 video=client_user_info['message_video_file_id'])
             await bot.send_document(int(client_user_info['chatid']), open(f"{client_user_info['local_video_in_file_path']}.sig", 'rb'),
             caption = f"Цифровая подпись к видео",
             )
